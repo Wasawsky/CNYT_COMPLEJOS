ace44ee1a9528aa3a58fe03ad27c2f3d8d4c3e59
unknown <2134093@labinfo.is.escuelaing.edu.co>
2019-09-03 09:31:23 -0500

    Pruebas Terminadas, Funciones completas, Agregado archivo de entradas manuales, Parte7

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,4 @@
-#Numeros complejos
+#Calculadora de Numeros complejos
 #MICHAEL BALLESTEROS
 #2019-2
 
@@ -133,10 +133,6 @@ def accion(m1,v1):
             y hallo la accion de la matriz sobre el vector   -> vector complejo
     """
     return multiplicacionMatrizMatriz(m1,transpuesta([v1]))
-def productoInternoVector(v1,v2):
-    """ Recibo 2 vectores de n*1 y calculo el producto interno -> numero complejo
-    """
-    return multiplicacionMatrizMatriz(matrizConjugada([v1]),transpuesta([v2]))[0][0]
 def sumaDiagonal(m1):
     """ Recibo una matriz de n*n y calculo la suma de los elementos diagonales -> numero complejo
     """
@@ -148,20 +144,10 @@ def productoInternoMatriz(m1,m2):
     """ Recibo 2 matrices complejas y calculo el producto interno -> numero complejo
     """
     return sumaDiagonal(multiplicacionMatrizMatriz(matrizAdjunta(m1),m2))
-def normaVector(v1):
-    """ Recibo 1 vector de n*1 y hallo su norma -> numero real
-    """
-    print(productoInternoVector(v1,v1)[0])
-    return math.sqrt(productoInternoVector(v1,v1)[0])
 def normaMatriz(m1):
     """ Recibo 1 matriz de n*m y hallo su norma -> numero real
     """
     return math.sqrt(productoInternoMatriz(m1,m1)[0])
-def distanciaVectorVector(v1,v2):
-    """ Recibo 2 vectores y determino su distancia -> numero real
-    """
-    print(sumaVectores(v1,inversa(v2)))
-    return normaVector(sumaVectores(v1,inversa(v2)))
 def distanciaMatrizMatriz(m1,m2):
     """ Recibo 2 matrices y determino su distancia -> numero real
     """
@@ -196,89 +182,14 @@ def matrizHermitian(m1):
                 esHermitian=False
                 break
     return esHermitian
-    
-#[[(1,2),(2,3),(3,4)],[(1,2),(2,3),(3,4)]]
-
-
-#v1=[tuple(map(float, x.split(","))) for x in (input().split(" "))]
-#v2=[tuple(map(float, x.split(","))) for x in (input().split(" "))]
-"""""""""
-
-"""""""""
-
-
-"""""""""
-m1=[]
-m2=[]
-for i in range(2):
-    vf=[tuple(map(float, x.split(","))) for x in (input().split(" "))]
-    m1.append(vf)
-for i in range(2):
-    vf=[tuple(map(float, x.split(","))) for x in (input().split(" "))]
-    m2.append(vf)
-"""""""""
-#c = tuple(map(float,input().split(",")))
-#print(v1)
-#print(v2)
-#print(sumaVectores(v1,v2))
-#print(inversa(v1))
-#print(multiplicacionEscalarVector(v1,c))
-#print(sumaMatrices(m1,m2))
-#print(inversaMatriz(m1))
-#print(multiplicacionEscalarMatriz(m1,c))
-#print(transpuesta(m1))
-#print(matrizConjugada(m1))
-#print(matrizAdjunta(m1))
-#print(multiplicacionMatrizMatriz(m1,m2))
-#print(accion(m1,v1))
-#print(productoInternoVector(v1,v2))
-#print(sumaDiagonal(m1))
-#print(productoInternoMatriz(m1,m2))
-#print(normaVector(v1))
-#print(normaMatriz(m1))
-#print(distanciaVectorVector(v1,v2))
-#print(distanciaMatrizMatriz(m1,m2))
-#print(matrizUnitaria(m1))
-#print(matrizHermitian(m1))
-################################
-#c = tuple(map(float,input().split(",")))
-#c1 = tuple(map(int,input().split(",")))
-#c2 = tuple(map(int,input().split(",")))
-
-
-#print(suma(c1,c2))
-#print(resta(c1,c2))
-#print(multiplicacion(c1,c2))
-#print(division(c1,c2))
-#print(modulo(c))
-#print(conjugado(c))
-#print(convpolaracartesiano(c))
-#print(convcartesianoapolar(c))
-#print(fase(c))
-################################
-
-
-"""
-###ANOTACIONES
-
-FORMA DE VECTOR SENCILLA 1*N
-
-V = [V1,V2,V3,...VN]
-
-FORMA DE VECTOR N*1 EN MATRIZ
-
-M = [[V1],[V2],[V3],....,[VN]]
-
-FORMA DE VECTOR 1*N EN MATRIZ
-
-M = [[V1,V2,V3,...VN]]
-
-FORMA DE MATRIZ M*N
-
-MA=[MA1[MA11, MA12, MA13, .....MA1N],
-    MA2[MA21, MA22, MA23, .....MA1N],
-    MA3[MA31, MA32, MA33, .....MA1N],
-    ......,
-    MAM[MAM1, MAM2, MAM3,.....,MAMN]]
-
-"""
+def productoTensor(m1,m2):
+    """ Determino el producto tensor de dos matrices complejas -> matriz compleja
+    """
+    m=len(m1)
+    n=len(m2)
+    m3=[[(0,0) for x in range(len(m1[0])*len(m2[0]))] for x in range(m*n)]
+    for j in range(len(m3)):
+        for k in range(len(m3[0])):
+            m3[j][k]=multiplicacion(m1[j//n][k//m],m2[j%n][k%m])
+    return m3
+#---------------------------------------------------------------------
